55504dfff732bdea0c247eda50c1def2ce128c38
Adrian Veppo <veppoadrian98@gmail.com>
2024-08-25 15:08:58 -0300

    cuarto commit

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,20 @@
 import './header.css';
 import Icono from './assets/Logo_VerdeOscuro-removebg-preview.png';
 import { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom"
 
 function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
 
+    const history = useNavigate();
+
     return (
         <div className={`header ${menuOpen ? 'active' : ''}`}>
             <div className='icono'>
+            <Link to='/'>
                 <img src={Icono} className='icono' alt='logo'></img>
+            </Link>
+                
             </div>
             
             <button className='menu-toggle' onClick={() => setMenuOpen(!menuOpen)}>
@@ -16,10 +22,10 @@ function Header() {
             </button>
             
             <div className='opciones'>
-                <button>Acerca de</button>
-                <button>Citas</button>
-                <button>Servicios</button>
-                <button>Contacto</button>
+                <button onClick={()=>{history('/about')}}>Acerca de</button>
+                <button onClick={()=>{history('/citas')}}>Citas</button>
+                <button onClick={()=>{history('/servicios')}}>Servicios</button>
+                <button onClick={()=>{history('/contacto')}}>Contacto</button>
             </div>
 
             <button className='login'>Iniciar Sesión</button>
