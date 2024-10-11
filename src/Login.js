151f477d9efdebd699c79390265536f695dfd371
g-villa <94998774+g-villa@users.noreply.github.com>
2024-10-11 10:46:22 -0300

    MODIFICADO LOGIN.. PARA QUE AL LOGEARSE ADMIN/PROFEs/SECREs--> LLEVE DIRECTAMENTE A SUS PAGINAS

--- src/Login.js
+++ src/Login.js
@@ -29,33 +29,50 @@ function Login(){
         <div className="login_container">
             <h2>Iniciar sesión</h2>
 
-            <form>
+        {/* FORM MODIFICADO!!!!!!!!!!!!!!!!!!! PARA QUE AL LOGEARSE ADMIN/PROFEs/SECREs--> LLEVE DIRECTAMENTE A SUS PAGINAS */}
+        <form>
 
-                <h4>E-mail</h4>
+        <h4>E-mail</h4>
 
-                <input type='text' value={email} onChange={(e)=> {
-                    setEmail(e.target.value)
-                }}/>
+        <input type='text' value={email} onChange={(e)=> {
+            setEmail(e.target.value)
+        }}/>
 
-                <h4>Contraseña</h4>
+        <h4>Contraseña</h4>
 
-                <input type='password' value={password} onChange={e => {
-                    setPassword(e.target.value)
-                }}/>
+        <input type='password' value={password} onChange={e => {
+            setPassword(e.target.value)
+        }}/>
 
-                <button onClick={(e)=>{
-                    e.preventDefault()
+        <button onClick={(e)=>{
+            e.preventDefault()
 
-                    signInWithEmailAndPassword(auth, email, password)
-                    .then(res => {
-                        setUserFlag(true)
-                        history('/')})
-                    .catch(error => {
-                        setBoolLogin(true)
-                    })
+            signInWithEmailAndPassword(auth, email, password)
+            .then(res => {
+                setUserFlag(true)
 
-                }} className="login_button">Iniciar sesión</button>
-            </form>
+                // Verifica si el usuario es admin
+                if (email === "admin1@gmail.com") {
+                    history('/admin');  // Redirigir a la página de admin
+                }
+                // Verifica si el usuario es un profesional
+                else if (["profesional1@gmail.com", "profesional2@gmail.com", "profesional3@gmail.com", "profesional4@gmail.com"].includes(email)) {
+                    history('/profesionales');  // Redirigir a la página de profesionales
+                }
+                // Verifica si el usuario es secretaria
+                else if (email === "secre1@gmail.com") {
+                    history('/secretaria');  // Redirigir a la página de secretaria
+                }
+                else {
+                    history('/');  // Redirigir a la página principal para otros usuarios
+                }
+            })
+            .catch(error => {
+                setBoolLogin(true)
+            })
+
+        }} className="login_button">Iniciar sesión</button>
+        </form>
 
             <div className="login_fail">
                 <span>{boolLogin && 'Wrong e-mail and password'}</span>
