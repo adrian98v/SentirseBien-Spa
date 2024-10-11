05f8666c5ed141b11b63d5e463fc41e18ef23b5f
g-villa <94998774+g-villa@users.noreply.github.com>
2024-10-11 18:51:42 -0300

    MODIFICADOS BOTONES OPCIONES/ HEADER (TODO RESPONSIVE,) -MODIFICADO HEADER SECRE EL LOGO YA NO LLEVA A HOME Y BOTON SECRE LLEVA A PAGINA SECRE UNICAMENTE

--- src/HeaderSecre.jsx
+++ src/HeaderSecre.jsx
@@ -1,28 +1,21 @@
-import './HeaderProfes.css'; //header usa mismo css que profesionales
+import './HeaderProfes.css'; // header usa mismo css que profesionales
 import Icono from './assets/Logo_VerdeOscuro-removebg-preview.png';
-import { useState, useContext} from 'react';
-import { Link, useNavigate } from "react-router-dom"
+import { useState, useContext } from 'react';
+import { useNavigate } from "react-router-dom";
 import { DataContext } from './App.js';
-import { signOut } from 'firebase/auth'
+import { signOut } from 'firebase/auth';
 import { auth } from './firebase.js';
 
 function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
-
-    const {setUserFlag, userFlag, user} = useContext(DataContext);
-
+    const { setUserFlag, userFlag, user } = useContext(DataContext);
     const history = useNavigate();
 
-
-    
     return (
         <div className={`header ${menuOpen ? 'active' : ''}`}>
             <div className='icono'>
-                
-            <Link to='/'>
+                {/* Elimina el Link para que el logo no redirija */}
                 <img src={Icono} className='icono' alt='logo'></img>
-            </Link>
-                
             </div>
             
             <button className='menu-toggle' onClick={() => setMenuOpen(!menuOpen)}>
@@ -30,21 +23,20 @@ function Header() {
             </button>
             
             <div className='opciones'>
-                
-                <button onClick={() => {history('/admin')}}>SECTOR SECRETARIAS/OS </button>
-
+                {/* Cambia la ruta para que redirija a la página Secretaria */}
+                <button onClick={() => { history('/secretaria') }}>SECTOR SECRETARIAS/OS</button>
             </div>
 
-            <button className='login' onClick={()=>{
-
-                if(user){
-                    signOut(auth)
-                    setUserFlag(false)
-                }else{
-                    history('/login')
-                } 
-                
-                }}>{userFlag ? "Cerrar sesión" : "Iniciar sesión"}</button>
+            <button className='login' onClick={() => {
+                if (user) {
+                    signOut(auth);
+                    setUserFlag(false);
+                } else {
+                    history('/login');
+                }
+            }}>
+                {userFlag ? "Cerrar sesión" : "Iniciar sesión"}
+            </button>
         </div>
     );
 }
