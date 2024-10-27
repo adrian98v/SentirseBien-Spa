57de504ea070c0a4a99cf5add0bc3f6e207924ad
Adrian Veppo <veppoadrian98@gmail.com>
2024-10-27 20:17:46 -0300

    ajuste debito/credito en Mis Reservas

--- src/cliente-componentes-paginas/ClienteReservas.jsx
+++ src/cliente-componentes-paginas/ClienteReservas.jsx
@@ -229,24 +229,24 @@ function ClienteReservas() {
                                         sessionStorage.setItem('IDPendiente', reserva.id);
 
                                         const miServicio = reserva.servicio;
-                                        switch(miServicio){
-                                            case "Masaje AntiStress": window.location.href = "https://buy.stripe.com/test_aEUcPQaSm8NB5uE000"; break;
-                                            case "Masaje Circulatorio": window.location.href = "https://buy.stripe.com/test_28o7vwaSm8NB7CM5kl"; break;
-                                            case "Masaje Descontracturante": window.location.href = "https://buy.stripe.com/test_28o7vw7Ga5Bp0ak7su"; break;
-                                            case "Masaje c/Piedras": window.location.href = "https://buy.stripe.com/test_9AQ034gcG8NBf5e5kn"; break;
-                                            case "belleza Manos y Pies": window.location.href = "https://buy.stripe.com/test_6oEeXYf8C3thf5e5kq"; break;
-                                            case "belleza Depilacion Facial": window.location.href = "https://buy.stripe.com/test_eVa1781hMaVJf5e004"; break;
-                                            case "belleza Lifting Pestaña": window.location.href = "https://buy.stripe.com/test_5kAg22f8CfbZ1eo4gl"; break;
-                                            case "facial CrioFrecuencia Facial": window.location.href = "https://buy.stripe.com/test_7sI8zAe4ygg3bT2fZ5"; break;
-                                            case "facial LimpiezaProfunda+Hidr": window.location.href = "https://buy.stripe.com/test_9AQ4jkd0u1l96yIcMU"; break;
-                                            case "facial PuntaDiamnte": window.location.href = "https://buy.stripe.com/test_8wM8zA8Ke4xl3mw28h"; break;
-                                            case "corporal CrioFrecuencia Corpo": window.location.href = "https://buy.stripe.com/test_dR64jk5y29RF6yI5kx"; break;
-                                            case "corporal DermoHealth": window.location.href = "https://buy.stripe.com/test_5kAdTU4tY3th6yI00a"; break;
-                                            case "corporal Ultracavitacion": window.location.href = "https://buy.stripe.com/test_5kAeXY7Gad3RcX68wH"; break;
-                                            case "corporal VelaSlim": window.location.href = "https://buy.stripe.com/test_6oEeXYf8CbZNf5e00c"; break;
-                                        }
+                                        // switch(miServicio){
+                                        //     case "Masaje AntiStress": window.location.href = "https://buy.stripe.com/test_aEUcPQaSm8NB5uE000"; break;
+                                        //     case "Masaje Circulatorio": window.location.href = "https://buy.stripe.com/test_28o7vwaSm8NB7CM5kl"; break;
+                                        //     case "Masaje Descontracturante": window.location.href = "https://buy.stripe.com/test_28o7vw7Ga5Bp0ak7su"; break;
+                                        //     case "Masaje c/Piedras": window.location.href = "https://buy.stripe.com/test_9AQ034gcG8NBf5e5kn"; break;
+                                        //     case "belleza Manos y Pies": window.location.href = "https://buy.stripe.com/test_6oEeXYf8C3thf5e5kq"; break;
+                                        //     case "belleza Depilacion Facial": window.location.href = "https://buy.stripe.com/test_eVa1781hMaVJf5e004"; break;
+                                        //     case "belleza Lifting Pestaña": window.location.href = "https://buy.stripe.com/test_5kAg22f8CfbZ1eo4gl"; break;
+                                        //     case "facial CrioFrecuencia Facial": window.location.href = "https://buy.stripe.com/test_7sI8zAe4ygg3bT2fZ5"; break;
+                                        //     case "facial LimpiezaProfunda+Hidr": window.location.href = "https://buy.stripe.com/test_9AQ4jkd0u1l96yIcMU"; break;
+                                        //     case "facial PuntaDiamnte": window.location.href = "https://buy.stripe.com/test_8wM8zA8Ke4xl3mw28h"; break;
+                                        //     case "corporal CrioFrecuencia Corpo": window.location.href = "https://buy.stripe.com/test_dR64jk5y29RF6yI5kx"; break;
+                                        //     case "corporal DermoHealth": window.location.href = "https://buy.stripe.com/test_5kAdTU4tY3th6yI00a"; break;
+                                        //     case "corporal Ultracavitacion": window.location.href = "https://buy.stripe.com/test_5kAeXY7Gad3RcX68wH"; break;
+                                        //     case "corporal VelaSlim": window.location.href = "https://buy.stripe.com/test_6oEeXYf8CbZNf5e00c"; break;
+                                        // }
 
-
+                                        history('/opcionPago')
                                         
                                     }} // Asegúrate de implementar esta función
                                     >
