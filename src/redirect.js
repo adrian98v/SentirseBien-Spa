594fd596c3b1ec45ee4e52b3e0e3352797296a05
Adrian Veppo <veppoadrian98@gmail.com>
2024-11-05 16:46:50 -0300

    redirect cambio

--- src/redirect.js
+++ src/redirect.js
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react';
 
 function PaymentRedirect() {
-
-    
     useEffect(() => {
-        // Redirige al esquema personalizado
+        // Intenta la redirección automática
         window.location.href = "myapp://paymentConfirmation";
     }, []);
 
     return (
         <div style={styles.container}>
             <p style={styles.text}>Redirigiendo, por favor espera...</p>
+            {/* Enlace de respaldo para redirección manual */}
+            <a href="myapp://paymentConfirmation" style={styles.link}>
+                Si no ocurre nada, toca aquí para continuar.
+            </a>
         </div>
     );
 }
@@ -18,6 +20,7 @@ function PaymentRedirect() {
 const styles = {
     container: {
         display: 'flex',
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
         height: '100vh',
@@ -27,6 +30,12 @@ const styles = {
         fontSize: '1.2em',
         color: '#555',
     },
+    link: {
+        marginTop: '1em',
+        fontSize: '1em',
+        color: '#0066cc',
+        textDecoration: 'underline',
+    }
 };
 
 export default PaymentRedirect;
